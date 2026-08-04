d0a86fcf0e078b6b4709a8efc966e1333a12b8a3
MucaL00 <vasconcelloskelisamuel@gmail.com>
2026-08-04 14:32:57 -0300

    Aula3

--- Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
+++ Aula-01/PrimeiroProjeto/obj/Debug/net10.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27e174e23292b5d3e0311c2a9c83c9700d9c480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ea76c0fa9b5a9fd7d459c5e5a77072860944b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-03/Revisao/obj/Debug/net10.0/Revisao.AssemblyInfo.cs
+++ Aula-03/Revisao/obj/Debug/net10.0/Revisao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27e174e23292b5d3e0311c2a9c83c9700d9c480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ea76c0fa9b5a9fd7d459c5e5a77072860944b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
