bba0179cd92b7625ad1c0f5c46bafefef104760c
MucaL00 <vasconcelloskelisamuel@gmail.com>
2026-08-11 08:05:41 -0300

    Aula-04

--- Aula-03/Exercicios/Exercicio06/obj/Debug/net10.0/Exercicio06.AssemblyInfo.cs
+++ Aula-03/Exercicios/Exercicio06/obj/Debug/net10.0/Exercicio06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411c6ab495803d837affced2c53adaa1a3c5192e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58f1f249c5ab89a59499bef611e422875405b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
